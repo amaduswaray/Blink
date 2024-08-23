71eb7193c88cf18ee171a455600a9bcb8cc63a01
Amadu Swaray <86655546+amaduswaray@users.noreply.github.com>
2024-08-23 18:38:04 +0200

    Create catppuccin.js

---
+++ catppuccin.js
@@ -0,0 +1,24 @@
+// Catppuccin Mocha
+
+t.prefs_.set('color-palette-overrides', [
+  "#45475a", // black
+  "#f38ba8", // red
+  "#a6e3a1", // green
+  "#f9e2af", // yellow
+  "#89b4fa", // blue
+  "#f5c2e7", // magenta
+  "#94e2d5", // cyan
+  "#bac2de", // white
+  "#585b70", // bright black
+  "#f38ba8", // bright red
+  "#a6e3a1", // bright green
+  "#f9e2af", // bright yellow
+  "#89b4fa", // bright blue
+  "#f5c2e7", // bright magenta
+  "#94e2d5", // bright cyan
+  "#a6adc8"  // bright white
+]);
+
+t.prefs_.set('foreground-color', "#cdd6f4"); // foreground
+t.prefs_.set('background-color', "#1e1e2e"); // background
+t.prefs_.set('cursor-color', 'rgba(245,224,220,0.5)'); // cursor color in RGBA format for transparency
